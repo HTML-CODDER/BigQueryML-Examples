b529a7101207b086c505a945947207089c26c5a5
Gad Benram <6208212+gidutz@users.noreply.github.com>
2018-12-03 15:49:11 -0800

    Update geoviz.sql

--- AutoTel/geoviz.sql
+++ AutoTel/geoviz.sql
@@ -7,7 +7,7 @@ EXTRACT( HOUR FROM timestamp) hour,
 AVG(predicted_label) prediction,
 AVG(free_cars) free_cars,
 neighborhood_name
- from `autotel_demo.free_cars_predictions`  group by 1,2, neighborhood_name) as data
+ from `autotel_demo.free_cars_predictions_v1`  group by 1,2, neighborhood_name) as data
  JOIN autotel_demo.tel_aviv_neighborhood as neighs ON
  neighs.neighborhood_name = data.neighborhood_name
- WHERE hour=13 AND day = DATE '2018-11-25'
+ WHERE hour=13 AND day = DATE '2018-11-24'
