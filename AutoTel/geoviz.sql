670cafc464cad86c4b3f0d93ac47c975920b2ec7
Gad Benram <6208212+gidutz@users.noreply.github.com>
2018-12-03 15:53:19 -0800

    Update geoviz.sql

--- AutoTel/geoviz.sql
+++ AutoTel/geoviz.sql
@@ -10,4 +10,4 @@ neighborhood_name
  from `autotel_demo.free_cars_predictions_v1`  group by 1,2, neighborhood_name) as data
  JOIN autotel_demo.tel_aviv_neighborhood as neighs ON
  neighs.neighborhood_name = data.neighborhood_name
- WHERE hour=13 AND day = DATE '2018-11-24'
+ WHERE hour=14 AND day = DATE '2018-11-25'
