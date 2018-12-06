0f4582f9389e8d5dc5220e748de5e32610c21fc1
Gad Benram <6208212+gidutz@users.noreply.github.com>
2018-12-06 13:04:39 -0800

    Update geoviz.sql

--- AutoTel/geoviz.sql
+++ AutoTel/geoviz.sql
@@ -2,11 +2,11 @@ SELECT
 ST_GeogFromText(neighs.area_polygon) WKT ,
 data.*
 from (select
-DATE_TRUNC(cast (timestamp as DATE), DAY) day,
-EXTRACT( HOUR FROM timestamp) hour,
-AVG(predicted_label) prediction,
-AVG(free_cars) free_cars,
-neighborhood_name
+         DATE_TRUNC(cast (timestamp as DATE), DAY) day,
+         EXTRACT( HOUR FROM timestamp) hour,
+         ROUND(AVG(predicted_label), 1) prediction,
+         ROUND(AVG(free_cars), 1) free_cars,
+         neighborhood_name
  from `autotel_demo.free_cars_predictions_v1`  group by 1,2, neighborhood_name) as data
  JOIN autotel_demo.tel_aviv_neighborhood as neighs ON
  neighs.neighborhood_name = data.neighborhood_name
