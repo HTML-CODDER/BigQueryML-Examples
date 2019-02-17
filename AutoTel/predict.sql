db896517ab4093793f3756c17c50873d68d7bff2
Gad Benram <gidutz@gmail.com>
2019-02-17 13:42:01 +0200

    Update predict.sql

--- AutoTel/predict.sql
+++ AutoTel/predict.sql
@@ -1,3 +1,5 @@
+
+-- v1
 SELECT
   EXP(predicted_label) - 1 predicted_label
   ,free_cars
@@ -23,4 +25,27 @@ FROM
   `doit_intl_autotel_public.dataset_v1` as dataset
   WHERE dataset.TIMESTAMP > TIMESTAMP '2019-02-10'))
 
-  
+-- v2
+SELECT
+  EXP(predicted_label) - 1 predicted_label
+  ,free_cars
+  ,ABS(free_cars-predicted_label) ERR
+ , neighborhood_name
+ ,timestamp
+ FROM ML.PREDICT(MODEL `doit_intl_autotel_public.free_cars_model_v2`,
+
+(SELECT
+      age20to29
+    , age30to34
+    , age35to49
+    , age50to64
+    , total_kingrtn
+    , total_hotels
+    , hour
+    , is_workday
+    , free_cars
+    , neighborhood_name
+    , timestamp
+FROM
+  `doit_intl_autotel_public.dataset_v2` as dataset
+  WHERE dataset.TIMESTAMP > TIMESTAMP '2019-02-10'))
