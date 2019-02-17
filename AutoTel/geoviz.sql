deb6d2da372e05c1c40d75eeda4d2892a618156a
Gad Benram <gidutz@gmail.com>
2019-02-17 13:33:35 +0200

    Update geoviz.sql

--- AutoTel/geoviz.sql
+++ AutoTel/geoviz.sql
@@ -7,7 +7,7 @@ from (select
          ROUND(AVG(predicted_label), 1) prediction,
          ROUND(AVG(free_cars), 1) free_cars,
          neighborhood_name
- from `autotel_demo.free_cars_predictions_v1`  group by 1,2, neighborhood_name) as data
- JOIN autotel_demo.tel_aviv_neighborhood as neighs ON
+ from `doit_intl_autotel_public.prediction_v1`  group by 1,2, neighborhood_name) as data
+ JOIN doit_intl_autotel_public.tel_aviv_neighborhood as neighs ON
  neighs.neighborhood_name = data.neighborhood_name
- WHERE hour=14 AND day = DATE '2018-11-25'
+ WHERE hour=14 AND day = DATE '2019-02-13'
